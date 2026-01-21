18ef3f0e8e95dc48d2d23e2919683de8db609f48
sackoibz-cyber <sackoibz@gmail.com>
2026-01-21 14:17:44 +0000

    Update and rename commands/ping.js to commands/hex-bot/ping.js

--- commands/hex-bot/ping.js
+++ commands/hex-bot/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'ping',
+    category: 'hex-bot',
     description: 'Répond avec Pong!',
     execute: async (sock, sender) => {
         await sock.sendMessage(sender, { text: 'Pong!' });
     }
-};
+}; 
