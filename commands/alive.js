9214d9c137b3ec239bab24978915d749dd59e062
sackoibz-cyber <sackoibz@gmail.com>
2026-01-22 18:09:31 +0000

    Delete commands/alive.js

--- commands/alive.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'alive',
-    description: 'Vérifie si le bot est en ligne',
-    execute: async (sock, sender) => {
-        await sock.sendMessage(sender, { text: 'Je suis en ligne 😎' });
-    }
-}; 
