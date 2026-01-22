b43b88049a26436f4230a8baa3c4193d2f81abf1
sackoibz-cyber <sackoibz@gmail.com>
2026-01-22 18:13:27 +0000

    Delete commands/ninja.js

--- commands/ninja.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ninja',
-    description: 'Commande Ninja',
-    execute: async (sock, sender) => {
-        await sock.sendMessage(sender, { text: '🥷 Ninja activé!' });
-    }
-};
