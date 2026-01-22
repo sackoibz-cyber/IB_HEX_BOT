845f43aacd0f21d2b06e73a44a483ec5b57ae5ed
sackoibz-cyber <sackoibz@gmail.com>
2026-01-22 18:14:31 +0000

    Delete commands directory

--- commands/hex-bot/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    category: 'hex-bot',
-    description: 'Répond avec Pong!',
-    execute: async (sock, sender) => {
-        await sock.sendMessage(sender, { text: 'Pong!' });
-    }
-}; 
