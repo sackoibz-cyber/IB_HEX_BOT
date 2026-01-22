cec4949949d336d3e37176c16e92f49d8fc3f8c3
sackoibz-cyber <sackoibz@gmail.com>
2026-01-22 18:52:51 +0000

    Create dev.js

---
+++ commands/commands/hex-bot/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "dev",
+    description: "Informations sur le développeur",
+    execute: async (client, message, args) => {
+        message.reply(`🥷 Développeur du bot : sacko\nBot propulsé par IB_HEX_BOT`);
+    }
+};
